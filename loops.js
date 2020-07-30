4562cc23b8d93189e45e49e94f7e59ed5333a92d
Terrell Murrell <littlerell12@gmail.com>
2020-07-30 01:26:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,8 +19,14 @@ function whileLoop(n) {
     return "done";
 }
 
-function doWhileLoop(n) {
-    do {
-    console.log("I run once regardless.");
-  } while (incrementVariable() < 5);
+function doWhileLoop(array) {
+    function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+
 }
